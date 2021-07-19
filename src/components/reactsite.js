de4c18297c42f357cb993cce61c85ea139b0fedf
Annie <ikram.annie0@gmail.com>
2021-07-19 10:03:08 -0400

    react-site

--- src/components/reactsite.js
+++ src/components/reactsite.js
@@ -11,7 +11,7 @@ const Welcome = () => {
         <div className="welcome"> 
             <h2>Welcome to my React Site</h2>
             <input type="text" name="search" onChange={(event) => setName(event.target.value)} value={name}/>
-            <div className="greeting">{name ? `It's nice to meet you ${name}` : null}</div>
+            <div className="greeting">{name ? `Welcome to my react site ${name}` : null}</div>
             <a href="https://github.com/annie-i0"> My Github </a>
 
         </div>
